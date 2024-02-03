bcb7544e62e05d211d19e35bbca84cac9847c7e3
Andreas Ommundsen <anthomm@live.com>
2024-02-03 19:22:05 +0100

    feat: add ValidationPipe to app.useGlobalPipes;

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const VP = new ValidationPipe({
+  whitelist: true,
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(VP);
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap().then();
