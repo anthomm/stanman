9be67bdc201bca805e3bc8745e420f5361b3d664
Andreas Ommundsen <anthomm@live.com>
2024-02-03 22:09:23 +0100

    feat: implement users.service CRUD;

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // @InjectRepository(User) is needed because the DI system doesn't like generics.
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, update: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error(`User Not Found: ${id}`);
+    }
+    Object.assign(user, update);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error(`User Not Found: ${id}`);
+    }
+    return this.repo.remove(user);
+  }
+}
