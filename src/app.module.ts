d4f51ff8361448d2def5533938e43e846f6d3c0b
Andreas Ommundsen <anthomm@live.com>
2024-02-03 11:54:13 +0100

    nest g module users & reports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import { ReportsService } from './reports/reports.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService, UsersService, ReportsService],
 })
