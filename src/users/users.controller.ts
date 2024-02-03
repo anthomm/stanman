3eecf9d00830c91805b5feb3bc7bfc423b53df9a
Andreas Ommundsen <anthomm@live.com>
2024-02-03 23:05:02 +0100

    feat: add CRUDS to users.controller;

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -10,5 +20,28 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     const x = this.usersService.create(body.email, body.password);
     console.log(x);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    const pid = parseInt(id);
+    return this.usersService.findOne(pid);
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    const pid = parseInt(id);
+    return this.usersService.remove(pid);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    const pid = parseInt(id);
+    return this.usersService.update(pid, body);
   }
 }
