7f7034b6154f061289c5af1ffaad05c2a56db745
Andreas Ommundsen <anthomm@live.com>
2024-02-03 20:36:40 +0100

    fix: remove misplaced providers in app.module;

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
-import { ReportsService } from './reports/reports.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/entities/user.entity';
@@ -19,6 +17,6 @@ const TOM = TypeOrmModule.forRoot({
 @Module({
   imports: [UsersModule, ReportsModule, TOM],
   controllers: [AppController],
-  providers: [AppService, UsersService, ReportsService],
+  providers: [AppService],
 })
 export class AppModule {}
