04495bed2d31388778b093da444288bfc938af71
Andreas Ommundsen <anthomm@live.com>
2024-02-04 19:23:41 +0100

    feat: add cookie-session;

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const cookieSession = require('cookie-session');
 const VP = new ValidationPipe({
   whitelist: true,
 });
+const CS = cookieSession({ keys: ['DevKey'] });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(CS);
   app.useGlobalPipes(VP);
   await app.listen(3000);
 }
