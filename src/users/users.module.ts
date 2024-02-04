2e90093bd3a2cf8d7b3386e3144325cf62b430b9
Andreas Ommundsen <anthomm@live.com>
2024-02-04 16:40:43 +0100

    feat: add hashed signup;

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { AuthService } from './auth.service';
 
 const TOM = TypeOrmModule.forFeature([User]);
 
 @Module({
   imports: [TOM],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
