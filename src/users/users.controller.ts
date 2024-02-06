f101002a79b845fcee6052c4426cd341f6bb5723
Andreas Ommundsen <anthomm@live.com>
2024-02-06 09:49:56 +0100

    feat: users.controller: add /whoami handler;

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -39,7 +39,12 @@ export class UsersController {
     return user;
   }
 
-  @Get('/:id')
+  @Get('whoami')
+  whoami(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
+  }
+
+  @Get(':id')
   async findUser(@Param('id') id: string) {
     const pid = parseInt(id);
     const user = await this.usersService.findOne(pid);
